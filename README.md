f8dc124e53373c760c1a4cb120192beb4990f587
shiv <shubhamjoshi.shiv@gmail.com>
2021-08-09 11:33:44 +0530

    added url scanning functionality

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # fake_news_v_2
 version 2 of our fake news detection app
-verry nice
-l2
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import copy
 import pickle
 import streamlit as st
+import newspaper
 
 
 @st.cache(persist=True, allow_output_mutation=True)
@@ -16,6 +17,19 @@ def predict(text, trained_model):
     else:
         result = True
     return result
+
+
+def test_url(url, tm):
+    article = newspaper.Article(url=url)
+    try:
+        article.download()
+        article.parse()
+    except:
+        return (False, 'Could not fetch the URL')
+    text = article.text
+    if len(text) == 0:
+        return (False, 'Could not process the article')
+    return (predict(text, tm), "")
 
 
 def main():
@@ -36,11 +50,24 @@ def main():
             """
     st.markdown(hide_st_style, unsafe_allow_html=True)
 
-    news_text = st.text_area('plese enter the text of news here-')
+    news_text = st.text_area('please enter the text of news here-')
     if st.button("Predict"):
         result = predict(news_text, tm)
         st.write("the news seems to be", result)
         if result:
+            video_file = open('very_true.mkv', 'rb')
+            video_bytes = video_file.read()
+            st.video(video_bytes)
+
+        else:
+            video_file = open('fake_fake_disgusting_news.mp4', 'rb')
+            video_bytes = video_file.read()
+            st.video(video_bytes)
+    news_url = st.text_area('please enter the url of news here-')
+    if st.button("Predict from the url"):
+        result = test_url(news_url, tm)
+        st.write(result[1], "the news seems to be", result[0])
+        if result[0]:
             video_file = open('very_true.mkv', 'rb')
             video_bytes = video_file.read()
             st.video(video_bytes)
